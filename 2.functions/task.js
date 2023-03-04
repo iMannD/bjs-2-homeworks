c6a354384bfa5d3670213a2c4571bcd5fb3da535
iMannD <122105359+iMannD@users.noreply.github.com>
2023-03-04 20:58:51 +0300

    цикл прошел

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,15 @@
 function getArrayParams(...arr) {
-
+  let min = Infinity;
+  let max = -Infinity;
+  let sum;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    } else if (arr[i] > max) {
+      max = arr.max;
+    }
+    sum = sum + arr[i];
+  }
   return { min: min, max: max, avg: avg };
 }
 
