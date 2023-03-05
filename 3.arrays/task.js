e8a5b88f38b9a55c0a8e04f5fc3153082a78a2ff
iMannD <122105359+iMannD@users.noreply.github.com>
2023-03-05 22:11:42 +0300

    1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,5 +1,8 @@
 function compareArrays(arr1, arr2) {
-  
+  if (arr1.length != arr2.length) {
+    return false;
+  }
+  return arr1.every((a, b) => a === arr2[b]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
