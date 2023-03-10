a37d8285ec64f7e6d0275bf977efd492e77db4e5
iMannD <122105359+iMannD@users.noreply.github.com>
2023-03-10 22:59:52 +0300

    1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -2,15 +2,18 @@ function Student(name, gender, age) {
   name,
   gender,
   age,
-  marks = {},
+  marks = {}
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  subject = subjectName
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  this.marks = marks
+  if (this.marks !== undefined) {
+    this.marks.concat(marks)
+  }
 }
 
 Student.prototype.getAverage = function () {
