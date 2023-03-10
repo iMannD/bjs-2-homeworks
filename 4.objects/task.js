078d211b31b874da2bf9bceeb92efe23019c0594
iMannD <122105359+iMannD@users.noreply.github.com>
2023-03-10 23:13:26 +0300

    1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -16,9 +16,14 @@ Student.prototype.addMarks = function (...marks) {
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks === undefined) {
+    return 0;
+  }
+  return this.marks.reduce( (a, b) => a + b) / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.subject
+  delete this.marks
+  this.excluded = reason;
 }
