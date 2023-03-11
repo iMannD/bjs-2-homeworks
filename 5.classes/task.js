16abcdcdb109b702c513d072d25ac531558db13b
iMannD <122105359+iMannD@users.noreply.github.com>
2023-03-11 22:08:39 +0300

    1

---
+++ 5.classes/task.js
@@ -0,0 +1,9 @@
+class PrintEditionItem {
+    constructor (name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        state;
+        type;
+    }
+}
