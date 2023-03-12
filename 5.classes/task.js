8ae5bd490d9dc64b261dbe13a863cd986e03558d
iMannD <122105359+iMannD@users.noreply.github.com>
2023-03-12 19:28:28 +0300

    3

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -22,30 +22,11 @@ class PrintEditionItem {
         return this._state;
     }
 }
+
 class Magazine extends PrintEditionItem {
-    constructor(name, releaseDate, pagesCount) {
-        this.name = name;
-        this.releaseDate = releaseDate;
-        this.pagesCount = pagesCount;
-        this.state = 100;
         this.type = "magazine";
     }
-    fix() {
-        this.state = this.state * 1.5;
-    }
-    set state(state) {
-        if (this.state < 0) {
-            this._state = 0;
-        } else if (this.state > 100) {
-            this._state = 100;
-        } else {
-            this._state = state;
-        }
-    }   
-    get state() {
-        return this._state;
-    }
-}
+ 
 class Book extends PrintEditionItem {
     constructor(name, releaseDate, pagesCount, author) {
         this.name = name;
@@ -71,3 +52,4 @@ class Book extends PrintEditionItem {
         return this._state;
     }
 }
+class NovelBook 
