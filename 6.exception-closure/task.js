2330a8304b73b853f773e294d20e19b4f238a541
iMannD <122105359+iMannD@users.noreply.github.com>
2023-03-20 14:34:43 +0300

    1/2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,16 +1,21 @@
-﻿function parseCount(c) {
-    if (Number.parseFloat(c) = NaN) {
+﻿function parseCount(cc) {
+    if (Number.parseFloat(cc) = NaN) {
         throw new Error("Невалидное значение");
         return;
     }
-return parseFloat(c);
+return parseFloat(cc);
 }
 
-function validateCount(n) {
+function validateCount(nn) {
     try {
-        return parseCount(n)
+        return parseCount(nn)
     } catch (err) {
         return err;
     }
-    return parseCount(n);
+    return parseCount(nn);
+}
+
+
+class triangle {
+    constructor (a, b, c)
 }
