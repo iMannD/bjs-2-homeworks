70b4bc9d3c695e1a59466a2cab0c96a5d4699e85
iMannD <122105359+iMannD@users.noreply.github.com>
2023-03-24 22:56:11 +0300

    3

--- 7.async/task.js
+++ 7.async/task.js
@@ -5,15 +5,21 @@ class AlarmClock {
     }
     addClock(time, callback) {
         if (arguments.length < 2) {
-            throw new Error('Отсутствуют обязательные аргументы')
+            throw new Error('Отсутствуют обязательные аргументы');
         }
         if (this.alarmCollection.includes(alarmTime)) {
             console.warn('Уже присутствует звонок на это же время');
         }
-        this.alarmCollection.push {
+        this.alarmCollection.push({
             callback,
             time,
             canCall = true,
-        }
+        });
     }
+    removeClock(time) {
+        let timeIndex;
+        timeIndex = this.alarmCollection.indexOf(time);
+        if (timeIndex > -1) {
+            this.alarmCollection.splice(timeIndex, 1);
+        }
 }   
