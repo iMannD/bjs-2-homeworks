b99c42d529109e2e0f128ec1900c57cd1a23b99b
iMannD <122105359+iMannD@users.noreply.github.com>
2023-04-03 00:54:14 +0300

    right

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -3,12 +3,34 @@ function cachingDecoratorNew(func) {
   
 }
 
+function cachingDecoratorNew(func) {
+  let cache = [];
+  
+  function wrapper(...args) {
+      const hash = ???; // получаем правильный хеш c помощью функции md5
+      let objectInCache = cache.find((item) => ???); // ищем элемент, хеш которого равен нашему хешу
+      if (objectInCache) { // если элемент найден
+          console.log("Из кэша: " + ???); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+          return "Из кэша: " + ???;
+      }
+  
+      let result = func(...args); // в кеше результата нет — придётся считать
+      cache.push(???) ; // добавляем элемент с правильной структурой
+      if (cache.length > 5) { 
+        ??? // если слишком много элементов в кеше, надо удалить самый старый (первый) 
+      }
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;  
+  }
+  return wrapper;
+  }
+
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
   
 
 
-function cachingDecoratorNew(func) {
+/* function cachingDecoratorNew(func) {
   let cache = [];
 
 function wrapper(...args) {
@@ -28,7 +50,7 @@ function wrapper(...args) {
     return "Вычисляем: " + result;  
 }
 return wrapper;
-}
+} */
 
 
 function debounceDecoratorNew(func, ms) {  
