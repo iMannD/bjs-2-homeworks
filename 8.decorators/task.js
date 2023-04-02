e4a4c4df786d90cb8cead18fa9ec14639f4c69af
iMannD <122105359+iMannD@users.noreply.github.com>
2023-04-03 01:45:18 +0300

    33

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -10,8 +10,8 @@ function cachingDecoratorNew(func) {
       const hash = md5(args); // получаем правильный хеш c помощью функции md5
       let objectInCache = cache.find((item) => item.hash === hash); // ищем элемент, хеш которого равен нашему хешу
       if (objectInCache) { // если элемент найден
-          console.log("Из кэша: " + item.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-          return "Из кэша: " + item.value
+          console.log("Из кэша: " + objectInCache.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+          return "Из кэша: " + objectInCache.value
       }
   
       let result = func(...args); // в кеше результата нет — придётся считать
@@ -28,29 +28,6 @@ function cachingDecoratorNew(func) {
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
   
-
-
-/* function cachingDecoratorNew(func) {
-  let cache = [];
-
-function wrapper(...args) {
-    const hash = args.join(','); // получаем правильный хэш
-    let idx = cache.findIndex((item)=> item.hash === hash ); // ищем элемент, хэш которого равен нашему хэшу
-    if (idx !== -1 ) { // если элемент не найден
-        console.log("Из кэша: " + cache[idx].result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-        return "Из кэша: " + cache[idx].result;
-    }
-
-    let result = func(...args); // в кэше результата нет - придётся считать
-    cache.push({hash, result}) ; // добавляем элемент с правильной структурой
-    if (cache.length > 5) { 
-      cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый) 
-    }
-    console.log("Вычисляем: " + result);
-    return "Вычисляем: " + result;  
-}
-return wrapper;
-} */
 
 
 function debounceDecoratorNew(func, ms) {  
